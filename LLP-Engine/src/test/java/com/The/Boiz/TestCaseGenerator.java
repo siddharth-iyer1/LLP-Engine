310701df717f4eb9d0e498e965661b87c3729989
Siddharth <siddharth.iyer123@gmail.com>
2023-10-18 20:45:06 -0500

    Added create complete graph, might be problems, i love readability

--- LLP-Engine/src/test/java/com/The/Boiz/TestCaseGenerator.java
+++ LLP-Engine/src/test/java/com/The/Boiz/TestCaseGenerator.java
@@ -5,9 +5,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 import java.util.HashMap;
+import java.util.HashSet;
+
 import com.The.Boiz.SequentialSolver;
+
+// I was having issues writing the file to the tests folder so I just wrote the files then moved them.
 
 public class TestCaseGenerator {
 
@@ -16,6 +22,8 @@ public class TestCaseGenerator {
     public static void main(String[] args) {
         HashMap<List<Integer>, List<Integer>> scanTestCases = generateScanTestCases(50);
         writeToScanTextFile(scanTestCases, "scanTestCases.txt");
+        HashMap<List<List<Integer>>, List<Integer>> graphTestCases = generateCompleteGraphTestCases(50);
+
     }
 
     public static HashMap<List<Integer>, List<Integer>> generateScanTestCases(int numberOfTestCases) {
@@ -71,5 +79,57 @@ public class TestCaseGenerator {
             e.printStackTrace();
         }
     }
-    
+
+    public static void writeToGraphTextFile(HashMap<List<List<Integer>>, List<Integer>> graphTestCases, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for(List<List<Integer>> graph : graphTestCases.keySet()) {
+                writer.write(graph.toString());
+                writer.newLine();
+                writer.newLine();
+
+                writer.write("Expected Output, Prim's: ");
+                List<Integer> output = graphTestCases.get(graph);
+                writer.write(output.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Write me a java program that generates 50 complete graphs with 10-25 vertices and writes them to a file called completeGraphTestCases.txt
+    public static HashMap<List<List<Integer>>, List<Integer>> generateCompleteGraphTestCases(int numberOfTestCases) {
+        HashMap<List<List<Integer>>, List<Integer>> testCases = new HashMap<>();
+        for (int i = 0; i < numberOfTestCases; i++) {
+            int vertices = 10 + RANDOM.nextInt(16); // Random number between 10 and 25
+            List<List<Integer>> graph = generateCompleteGraph(vertices);
+            testCases.put(graph, SequentialSolver.seqPrims(graph));
+
+        }
+        return testCases;
+    }
+
+    private static List<List<Integer>> generateCompleteGraph(int vertices) {
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        
+        // To ensure unique edge weights
+        Set<Integer> usedWeights = new HashSet<>();
+
+        for (int i = 0; i < vertices; i++) {
+            List<Integer> edges = new ArrayList<>();
+            for (int j = i + 1; j < vertices; j++) {
+                int weight;
+                do {
+                    weight = RANDOM.nextInt(1000); // Assuming edge weights between 0-999
+                } while (usedWeights.contains(weight));
+                usedWeights.add(weight);
+                
+                edges.add(weight);
+            }
+            adjacencyList.add(edges);
+        }
+
+        return adjacencyList;
+    }
 }
+
